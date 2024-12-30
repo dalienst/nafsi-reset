69ab103f7e80c4fc5e49e64ce995aa65ff46a1bb
dalienst <oduordalienst@gmail.com>
2024-12-30 16:17:49 +0300

    update settings

--- app/(routes)/settings/page.jsx
+++ app/(routes)/settings/page.jsx
@@ -1,30 +1,73 @@
 "use client";
-import { Formik } from "formik";
+import { useFetchProfile } from "@/hooks/accounts/actions";
+import useAxiosAuth from "@/hooks/useAxiosAuth";
+import useUserId from "@/hooks/useUserId";
+import { updateProfile } from "@/services/accounts";
+import { Field, Form, Formik } from "formik";
 import Link from "next/link";
 import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 function Settings() {
   const [loading, setLoading] = useState(false);
+  const axios = useAxiosAuth();
+  const userId = useUserId();
+
+  const {
+    isLoading: isLoadingProfile,
+    data: profile,
+    refetch: refetchProfile,
+  } = useFetchProfile();
 
   return (
-    <div className="container py-3">
+    <div className="auth-screen">
+      <Formik
+        initialValues={{
+          name: profile?.name || "",
+        }}
+        onSubmit={async (values) => {
+          setLoading(true);
+          try {
+            const formData = new FormData();
+            formData.append("name", values.name);
 
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item">
-            <Link href="/dashboard">Dashboard</Link>
-          </li>
-          <li className="breadcrumb-item active" aria-current="page">
-            Account Settings
-          </li>
-        </ol>
-      </nav>
+            await updateProfile(userId, formData, axios);
+            toast?.success("Profile updated successfully");
+            refetchProfile();
+          } catch (error) {
+            console.log(error?.response?.data);
+            toast?.error("Profile update failed");
+          } finally {
+            setLoading(false);
+          }
+        }}
+      >
+        {({ touched }) => (
+          <Form className="shadow p-4 bg-white form">
+            <h2 className="text-center mb-4">Account Settings</h2>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <Field
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                placeholder={profile?.name || "Enter your name"}
+              />
+            </div>
 
-      <h1 className="h2">Account Settings</h1>
-
-      <section className="mt-3 mb-3">
-        <Formik initialValues={{}}></Formik>
-      </section>
+            <button
+              type="submit"
+              className="btn btn-success"
+              disabled={loading}
+            >
+              {loading ? "Updating..." : "Update"}
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
