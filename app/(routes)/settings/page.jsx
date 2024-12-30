77a8c6c6e63e78a2bd9543c07524cc582a0736f2
dalienst <oduordalienst@gmail.com>
2024-12-30 15:51:10 +0300

    work on settings page

--- app/(routes)/settings/page.jsx
+++ app/(routes)/settings/page.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
+import { Formik } from "formik";
+import Link from "next/link";
+import React, { useState } from "react";
 
 function Settings() {
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>Settings</div>
-  )
+    <div className="container py-3">
+
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Account Settings
+          </li>
+        </ol>
+      </nav>
+
+      <h1 className="h2">Account Settings</h1>
+
+      <section className="mt-3 mb-3">
+        <Formik initialValues={{}}></Formik>
+      </section>
+    </div>
+  );
 }
 
-export default Settings
+export default Settings;
