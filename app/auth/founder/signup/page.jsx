d47538551550c6a42201cf4cfc78d9e53eb4b9e5
dalienst <oduordalienst@gmail.com>
2024-12-30 15:18:47 +0300

    added auth forms

---
+++ app/auth/founder/signup/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FounderSignUp() {
+  return (
+    <div>FounderSignUp</div>
+  )
+}
+
+export default FounderSignUp
---
+++ app/auth/login/page.jsx
@@ -0,0 +1,89 @@
+"use client";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+
+function Login() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const response = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (response?.error === "CredentialsSignin") {
+      toast.error("Invalid email or password");
+    } else {
+      toast.success("Login successful");
+      router.push("/dashboard");
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="auth-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="shadow p-4 bg-white rounded form"
+      >
+        <h2 className="text-center mb-4">Welcome Back</h2>
+
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Enter Email
+          </label>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control rounded-0"
+            required
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Enter Password
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control rounded-0"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-dark rounded-0 w-100"
+          disabled={loading}
+        >
+          {loading ? "Logging in..." : "Login"}
+        </button>
+
+        <div className="text-center mt-3">
+          <p>
+            Forgot Password?{" "}
+            <Link href="/auth/forgot-password" className="text-primary">
+              Reset Password
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
