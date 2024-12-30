c3c634beb4f7716c40913e3bde720c7fe1dba8b8
dalienst <oduordalienst@gmail.com>
2024-12-30 17:06:40 +0300

    create hooks for fetching details

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/settings/:path*"],
+  matcher: ["/dashboard/:path*", "/settings/:path*", "/clients/:path*"],
 };
