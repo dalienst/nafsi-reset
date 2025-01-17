6e7ddccb847285eceaef446398e54667e1023cd2
dalienst <oduordalienst@gmail.com>
2025-01-17 11:59:05 +0300

    added co-founder

--- data/about/team.jsx
+++ data/about/team.jsx
@@ -12,6 +12,18 @@ const team = [
     experience: `I have extensive experience supporting women refugees from Congo, Rwanda, and Somalia, 
     and have worked with the National Council of Persons with Disabilities to promote disability inclusion and mental health awareness.`,
   },
+  {
+    id: 2,
+    name: "Dalienst Oduor",
+    role: "Co-Founder & Chief Technology Officer (CTO)",
+    image: "/dal.jpg",
+    qualifications: "Computer Science",
+    bio: `My primary focus is ensuring that all our systems are up and running seamlessly to provide 
+    the best experience for our clients. I manage critical functionalities such as booking, 
+    session scheduling, and maintaining data protection, which is a top priority for me to ensure that client information is always secure and confidential.`,
+    experience: `I’m also deeply committed to incorporating client feedback into improving our systems and processes, ensuring we continuously meet and exceed expectations. 
+    Additionally, I oversee our digital media efforts, helping Nafsi Reset reach more people and make mental health support accessible to all.`,
+  },
 ];
 
 export { team };
